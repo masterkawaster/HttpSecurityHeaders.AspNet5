68196c8dcac61c768f6c71f6852d37a299e3a053
masterkawaster <coffeedotnet@gmail.com>
2017-02-02 02:08:58 +0100

    Initial

---
+++ HttpSecurityHeaders.MVC/HttpSecurityHeaders.MVC/content/Controllers/ErrorPagesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class ErrorPagesController : Controller
+    {
+        // GET: Default
+        public ActionResult error()
+        {
+            return View();
+        }
+
+		public ActionResult 404()
+		{
+			return View();
+		}
+	}
+}
