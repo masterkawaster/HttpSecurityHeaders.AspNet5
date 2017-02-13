3a799ef58f3a0e7a0e475fbcd5556b97518049d5
masterkawaster <coffeedotnet@gmail.com>
2017-02-14 00:38:23 +0100

    Hpkp header added

---
+++ HttpSecurityHeaders.AspNet5/HttpSecurityHeaders.AspNet5/content/App_Code/HpkpModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace HttpSecurityHeaders.AspNet5
+{
+	public class HpkpModule : IHttpModule
+	{
+		private static string certPrimaryHash = WebConfigurationManager.AppSettings["CertPrimaryHash"];
+		private static string certSecondaryHash = WebConfigurationManager.AppSettings["CertSecondaryHash"];
+		private static string expireTime = WebConfigurationManager.AppSettings["ExpireTime"];
+		private static string reportUri = WebConfigurationManager.AppSettings["ReportUri"];
+
+
+		public void Init(HttpApplication context)
+		{
+			context.PreSendRequestHeaders += Context_PreSendRequestHeaders;
+		}
+
+		private void Context_PreSendRequestHeaders(object sender, EventArgs e)
+		{
+			if(!string.IsNullOrEmpty(certPrimaryHash) &&
+			   !string.IsNullOrEmpty(certSecondaryHash) &&
+			   !string.IsNullOrEmpty(expireTime))
+				HttpContext.Current.Response.Headers.Add("Public-Key-Pins",
+					$"pin-sha256=\"{certPrimaryHash}\"; " +
+					$"pin-sha256=\"{certSecondaryHash}\"; " +
+					$"max-age={expireTime}; " +
+					$"includeSubdomains; " +
+					$"report-uri=\"{reportUri}\"");
+		}
+
+		public void Dispose()
+		{
+			
+		}
+	}
+}
